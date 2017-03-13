0511c7e364e23e502f2ea895538d29b1149cd180
Nicholas Law <nick@nicholaslaw.com.au>
2017-03-14 06:36:58 +1100

    Add addition error methods

--- error.go
+++ error.go
@@ -9,14 +9,44 @@ type ErrorResponse struct {
 	Message string `json:"message"`
 }
 
-// UnprocessableEntity returns a 422 Unprocessable Entity json response
+// BadRequest returns a 400 Bad Request JSON response
+func (resp *HTTPResponse) BadRequest(message string) {
+	resp.WriteErrorResponse(http.StatusBadRequest, message)
+}
+
+// Unauthorized returns a 401 Unauthorized JSON response
+func (resp *HTTPResponse) Unauthorized(message string) {
+	resp.WriteErrorResponse(http.StatusUnauthorized, message)
+}
+
+// Forbidden returns a 401 Forbidden JSON response
+func (resp *HTTPResponse) Forbidden(message string) {
+	resp.WriteErrorResponse(http.StatusForbidden, message)
+}
+
+// NotFound returns a 404 Not Found JSON response
+func (resp *HTTPResponse) NotFound(message string) {
+	resp.WriteErrorResponse(http.StatusNotFound, message)
+}
+
+// MethodNotAllowed returns a 405 Method Not Allowed JSON response
+func (resp *HTTPResponse) MethodNotAllowed(message string) {
+	resp.WriteErrorResponse(http.StatusMethodNotAllowed, message)
+}
+
+// UnprocessableEntity returns a 422 Unprocessable Entity JSON response
 func (resp *HTTPResponse) UnprocessableEntity(message string) {
 	resp.WriteErrorResponse(http.StatusUnprocessableEntity, message)
 }
 
-// Conflict returns a 409 Conflict json response
+// Conflict returns a 409 Conflict JSON response
 func (resp *HTTPResponse) Conflict(message string) {
 	resp.WriteErrorResponse(http.StatusConflict, message)
+}
+
+// InternalServerError returns a 500 Internal Server Error JSON response
+func (resp *HTTPResponse) InternalServerError(message string) {
+	resp.WriteErrorResponse(http.StatusInternalServerError, message)
 }
 
 // WriteErrorResponse is the error response writer
--- error_test.go
+++ error_test.go
@@ -8,6 +8,131 @@ import (
 	resp "github.com/nicklaw5/go-respond"
 )
 
+func TestBadRequest(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.BadRequest("Bad request")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusBadRequest)
+	}
+
+	expected := `{"success":false,"code":400,"message":"Bad request"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestUnauthorized(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.Unauthorized("Unauthorized")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusUnauthorized)
+	}
+
+	expected := `{"success":false,"code":401,"message":"Unauthorized"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestForbidden(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.Forbidden("Forbidden")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusForbidden {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusForbidden)
+	}
+
+	expected := `{"success":false,"code":403,"message":"Forbidden"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.NotFound("Not found")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusNotFound)
+	}
+
+	expected := `{"success":false,"code":404,"message":"Not found"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestMethodNotAllowed(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.MethodNotAllowed("Method not allowed")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusMethodNotAllowed)
+	}
+
+	expected := `{"success":false,"code":405,"message":"Method not allowed"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
 func TestUnprocessableEntity(t *testing.T) {
 	req, err := http.NewRequest("POST", "/", nil)
 	if err != nil {
@@ -52,6 +177,31 @@ func TestConflict(t *testing.T) {
 	}
 
 	expected := `{"success":false,"code":409,"message":"An error occured"}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v wanted %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestInternalServerError(t *testing.T) {
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		res := resp.NewResponse(w)
+		res.InternalServerError("An unexpected error occured")
+	})
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("Handler returned wrong status code: got %v wanted %v",
+			status, http.StatusInternalServerError)
+	}
+
+	expected := `{"success":false,"code":500,"message":"An unexpected error occured"}`
 	if rr.Body.String() != expected {
 		t.Errorf("Handler returned unexpected body: got %v wanted %v",
 			rr.Body.String(), expected)
